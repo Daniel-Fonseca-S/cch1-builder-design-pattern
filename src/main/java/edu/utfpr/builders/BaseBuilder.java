d148b8e58dca4fb9ae45c3d607de6d9bffb613c2
Daniel Fonseca <101329358+Daniel-Fonseca-S@users.noreply.github.com>
2023-11-01 21:19:02 -0300

    Aumentando complexidade e escalabilidade do builder

--- src/main/java/edu/utfpr/builders/BaseBuilder.java
+++ src/main/java/edu/utfpr/builders/BaseBuilder.java
@@ -8,29 +8,29 @@ import edu.utfpr.enums.AcucarEnum;
  * @author Daniel Fonseca <danielfonseca@alunos.utfpr.edu.br>
  */
 public interface BaseBuilder {
-    public BaseBuilder nome(String nome);
+    BaseBuilder nome(String nome);
 
-    public BaseBuilder descricao(String descricao);
+    BaseBuilder descricao(String descricao);
 
-    public BaseBuilder preco(double preco);
+    BaseBuilder preco(double preco);
 
-    public BaseBuilder calorias(int calorias);
+    BaseBuilder calorias(int calorias);
 
-    public BaseBuilder tempoPreparo(int tempoPreparo);
+    BaseBuilder tempoPreparo(int tempoPreparo);
 
-    public BaseBuilder isEntrada(boolean isEntrada);
+    BaseBuilder isEntrada(boolean isEntrada);
 
-    public BaseBuilder isPratoPrincipal(boolean isPratoPrincipal);
+    BaseBuilder isPratoPrincipal(boolean isPratoPrincipal);
 
-    public BaseBuilder isBebida(boolean isBebida);
+    BaseBuilder isBebida(boolean isBebida);
 
-    public BaseBuilder isSobremesa(boolean isSobremesa);
+    BaseBuilder isSobremesa(boolean isSobremesa);
 
-    public BaseBuilder isVegetariano(boolean isVegetariano);
+    BaseBuilder isVegetariano(boolean isVegetariano);
 
-    public BaseBuilder isGlutenFree(boolean isGlutenFree);
+    BaseBuilder isGlutenFree(boolean isGlutenFree);
 
-    public BaseBuilder isLactoseFree(boolean isLactoseFree);
+    BaseBuilder isLactoseFree(boolean isLactoseFree);
 
-    public BaseBuilder taxaAcucar(AcucarEnum taxaAcucar);
+    BaseBuilder taxaAcucar(AcucarEnum taxaAcucar);
 }
